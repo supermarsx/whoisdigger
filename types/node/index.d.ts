9a3c2474e677f2cb7bd3c46a24dbe217826df417
Mariana M <github_618a@imariana.com>
2025-06-22 23:56:14 +0100

    Add path and dns stubs and BrowserWindow webContents

--- types/node/index.d.ts
+++ types/node/index.d.ts
@@ -6,9 +6,34 @@ declare module 'fs' {
   export function writeFileSync(path: string, data: string): void;
   export function createReadStream(path: string): any;
 }
+declare module 'path' {
+  export function join(...paths: string[]): string;
+}
+declare module 'dns' {
+  export function resolve(
+    hostname: string,
+    rrtype: string,
+    callback: (err: any, addresses: any) => void
+  ): void;
+}
 declare module 'readline' {
   export function createInterface(options: any): any;
 }
 declare module 'url' {
   export function format(urlObject: any): string;
 }
+
+interface NodeModule {
+  exports: any;
+  require: any;
+  id: string;
+  filename: string;
+  loaded: boolean;
+  parent: NodeModule | null;
+  children: NodeModule[];
+  paths: string[];
+}
+
+declare var module: NodeModule;
+
+export { NodeModule };
