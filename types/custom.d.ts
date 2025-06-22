077c4f5c04c7d0e17de9238478af6310c9fbc1c0
Mariana M <github_618a@imariana.com>
2025-06-22 21:52:24 +0100

    refactor: convert main script to TypeScript

--- types/custom.d.ts
+++ types/custom.d.ts
@@ -2,10 +2,28 @@ declare const require: any;
 declare module 'fs' {
   export function existsSync(path: string): boolean;
   export function readFileSync(path: string, options?: any): string;
+  export function readFile(path: string, callback: (err: any, data: string) => void): void;
   export function writeFileSync(path: string, data: string): void;
+}
+declare module 'url' {
+  export function format(urlObject: any): string;
 }
 declare module 'electron' {
   export const app: any;
+  export class BrowserWindow {
+    constructor(options?: any);
+    loadURL(url: string): void;
+    show(): void;
+    once(event: string, listener: (...args: any[]) => void): void;
+    on(event: string, listener: (...args: any[]) => void): void;
+    minimize(): void;
+    toggleDevTools(): void;
+  }
+  export const BrowserWindow: any;
+  export const Menu: any;
+  export interface IpcMainEvent {}
+  export const ipcMain: any;
+  export const dialog: any;
   export const remote: any;
 }
 declare module 'debug' {
@@ -28,4 +46,8 @@ declare module 'whois' {
 declare module 'app/js/common/parseRawData' {
   const parseRawData: any;
   export default parseRawData;
+}
+
+interface String {
+  format(...args: any[]): string;
 }
