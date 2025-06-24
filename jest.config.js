df4ad371f0d46ef91ad34b52db0b846a9b57c673
Mariana M <github_618a@imariana.com>
2025-06-24 13:00:48 +0100

    chore: add babel transform for jest

--- jest.config.js
+++ jest.config.js
@@ -3,8 +3,10 @@ module.exports = {
   testEnvironment: 'node',
   testMatch: ['**/test/**/*.test.ts'],
   transform: {
-    '^.+\\.ts$': ['ts-jest', { tsconfig: 'tsconfig.json', diagnostics: false }]
+    '^.+\\.ts$': ['ts-jest', { tsconfig: 'tsconfig.json', diagnostics: false }],
+    '^.+\\.m?js$': 'babel-jest'
   },
+  transformIgnorePatterns: ['node_modules/(?!(change-case|html-entities)/)'],
   setupFiles: ['<rootDir>/test/jest.setup.ts'],
   collectCoverage: true,
   coverageDirectory: 'coverage'
