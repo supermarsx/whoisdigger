320c2814fdc05624d3cabd3d74e3a28602ad373d
Mariana M <github_618a@imariana.com>
2025-06-24 15:37:53 +0100

    Fix template loader path

--- app/ts/renderer/templateLoader.ts
+++ app/ts/renderer/templateLoader.ts
@@ -1,10 +1,15 @@
 import $ from 'jquery';
 import Handlebars from 'handlebars/runtime';
 
-export async function loadTemplate(selector: string, template: string, context: any = {}): Promise<void> {
-  const module = await import(`../compiled-templates/${template.replace(/\\.hbs$/, '.js')}`);
-  const precompiled = module.default || module;
-  const compiled = Handlebars.template(precompiled);
+export async function loadTemplate(
+  selector: string,
+  template: string,
+  context: any = {}
+): Promise<void> {
+  const module = await import(
+    `../../compiled-templates/${template.replace(/\.hbs$/, '.js')}`
+  );
+  const compiled = Handlebars.template(module.default);
   const html = compiled(context);
   $(selector).html(html);
 }
