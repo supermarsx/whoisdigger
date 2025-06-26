a72d0fcc884fb184d114559e9693dca78767e5d8
Mariana M <github_618a@imariana.com>
2025-06-26 18:23:48 +0100

    fix: load renderer via ESM

--- app/ts/mainPanel.ts
+++ app/ts/mainPanel.ts
@@ -1,15 +1,8 @@
-if (typeof module === 'object') {
-  (window as any).module = module;
-  // Prevent Electron from treating inline scripts as modules
-  (globalThis as any).module = undefined as any;
-}
-
-require('./common/fontawesome.js');
-
-(window as any).$ = (window as any).jQuery = require('jquery');
-if ((window as any).module) (globalThis as any).module = (window as any).module;
+import './common/fontawesome.js';
+import $ from 'jquery';
+(window as any).$ = (window as any).jQuery = $;
 
 // The main HTML file is precompiled and loaded directly, so loading
 // additional content at runtime is unnecessary.
-// require('./renderer/loadcontents.js');
-require('./renderer.js');
+// import './renderer/loadcontents.js';
+import './renderer.js';
