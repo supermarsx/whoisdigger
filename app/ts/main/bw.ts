8d888e93718f99de69d2cd2d5851db781917b0f7
Mariana M <github_618a@imariana.com>
2025-06-29 19:26:32 +0100

    test: refresh stats worker

--- app/ts/main/bw.ts
+++ app/ts/main/bw.ts
@@ -4,7 +4,7 @@ const debug = debugModule('main.bw');
 
 const { app, BrowserWindow, Menu, ipcMain, dialog, remote } = electron;
 
-import './bw/fileinput'; // File input
-import './bw/wordlistinput'; // Wordlist input
-import './bw/process'; // Process stage
-import './bw/export'; // Export stage
+import './bw/fileinput.js'; // File input
+import './bw/wordlistinput.js'; // Wordlist input
+import './bw/process.js'; // Process stage
+import './bw/export.js'; // Export stage
