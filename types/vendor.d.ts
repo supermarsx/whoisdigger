03af8eef21ab3d9d4674778d5aaaaa31c67f134b
Mariana M <github_618a@imariana.com>
2025-07-04 15:36:18 +0100

    refactor: remove cwd from renderer

---
+++ types/vendor.d.ts
@@ -0,0 +1,26 @@
+declare module '*vendor/jquery.js' {
+  const jq: typeof import('jquery');
+  export default jq;
+}
+
+declare module '*vendor/handlebars.runtime.js' {
+  import Handlebars from 'handlebars';
+  export default Handlebars;
+}
+declare module '*vendor/change-case.js' {
+  export * from 'change-case';
+}
+
+declare module '*vendor/html-entities/index.js' {
+  export * from 'html-entities';
+}
+
+declare module '*vendor/datatables.js' {
+  const d: any;
+  export default d;
+}
+
+declare module '*vendor/fontawesome.js' {
+  const d: any;
+  export default d;
+}
